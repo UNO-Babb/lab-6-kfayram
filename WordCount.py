51da7e506321079bbff07f32155bca1786d34908
kfayram <gafferkyle@hotmail.com>
2025-03-02 16:37:04 +0000

    figuring out how to create a loop to count each character in each line

--- WordCount.py
+++ WordCount.py
@@ -5,10 +5,30 @@
 
 def main():
   textFile = open("gettysberg.txt", 'r')
-  
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+    #letterCount = len(line) #for some reason this is only counting line 3 for character count count
+    letter = len(line)
+    for c in letter:
+      letterCount = letterCount + 1
+
+    print(letterCount)
+
+
+
+
+  print("Lines:", lineCount) 
+  print("Words:", wordCount)
+  print("Characters:", letterCount)
+
+ 
 
 if __name__ == '__main__':
   main()
